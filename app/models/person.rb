f8f9b30bd3076d3ae774551d7e29fc2f5c0a136e
Hyoseong Choi <rorlab@gmail.com>
2012-12-18 08:29:29 +0900

    form error message / locale / timezone

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,8 @@
 class Person < ActiveRecord::Base
   attr_accessible :gender, :mobile, :name, :contact_attributes
-  has_one :contact
+  has_one :contact, :dependent => :destroy
 
   accepts_nested_attributes_for :contact
+
+  validates :name, :presence => true
 end
